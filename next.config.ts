a829e8b73f9a6225c444a37bd4433f5161532dd7
s3rper <113769202+s3rper@users.noreply.github.com>
2026-02-20 12:10:34 +0800

    Added new tools medicare-calendar, medicare-matchmaker, client-success-stories and path-finder

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,26 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   basePath: "/app",
+  async rewrites() {
+    return [
+      {
+        source: '/client-success-stories',
+        destination: '/tools/client-success-stories.html',
+      },
+      {
+        source: '/deadline-reminder-tool',
+        destination: '/tools/deadline-reminder-tool.html',
+      },
+      {
+        source: '/match-an-advisor',
+        destination: '/tools/match-an-advisor.html',
+      },
+      {
+        source: '/path-finder',
+        destination: '/tools/path-finder.html',
+      },
+    ];
+  },
 };
 
 export default nextConfig;
